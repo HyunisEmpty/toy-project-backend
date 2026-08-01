8c3089713d2382e15ed737d940761597bb116e80
박현빈 <empty6004@kookmin.ac.kr>
2026-08-01 18:36:08 +0900

    feat: 로그인 비즈니스 로직 구현

--- backend/src/main/java/com/toyproject/backend/service/AuthService.java
+++ backend/src/main/java/com/toyproject/backend/service/AuthService.java
@@ -1,8 +1,8 @@
 package com.toyproject.backend.service;
 
 import com.toyproject.backend.domain.User;
-import com.toyproject.backend.dto.auth.SignupRequest;
-import com.toyproject.backend.dto.auth.SignupResponse;
+import com.toyproject.backend.dto.auth.*;
+import com.toyproject.backend.jwt.JwtProvider;
 import com.toyproject.backend.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
 
     public SignupResponse signup(SignupRequest request){
         if (userRepository.existsByUsername(request.getUsername())){
@@ -32,5 +33,28 @@ public class AuthService {
         userRepository.save(user);
 
         return new SignupResponse("회원가입이 완료되었습니다.");
+    }
+
+    public LoginResponse login(LoginRequest request){
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호가 올바르지 않습니다."));
+
+        // 비밀번호가 일치하는지 확인
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+            throw new IllegalArgumentException("아이디 또는 비밀번호가 올바르지 않습니다.");
+        }
+
+        // JwtProvider 클래스의 createToken함수 호출
+        String accessToken = jwtProvider.createToken(user.getId(), user.getUsername());
+
+        LoginUserResponse loginUser = new LoginUserResponse(
+                user.getId(),
+                user.getUsername()
+        );
+
+        return new LoginResponse(
+                accessToken,
+                loginUser
+        );
     }
 }

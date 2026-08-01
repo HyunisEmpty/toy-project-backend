87cdc16639a3701cf93ffe7b795095a9770417e8
박현빈 <empty6004@kookmin.ac.kr>
2026-08-01 18:19:50 +0900

    feat: JWT 토큰 생성 기능 구현

--- backend/src/main/java/com/toyproject/backend/jwt/JwtProvider.java
+++ backend/src/main/java/com/toyproject/backend/jwt/JwtProvider.java
@@ -1,33 +1,48 @@
 package com.toyproject.backend.jwt;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+// Spring Bean으로 등록
 @Component
+// JWT를 생성하고, 이후 검증까지 담당하는 클래스
 public class JwtProvider {
 
+    // Value 어노테이션은 application.properties에 저장된 값을 불러 올 수 있음
     @Value("${jwt.secret}")
-    private String secretKey;
+    private String secret;
 
     @Value("${jwt.expiration}")
-    private Long expiration;
+    private long expiration;
 
-    public String createToken(Long userId, String username){
+    private SecretKey secretKey;
+
+    // 빈생성이 끝난 직후 한 번만 실행되는 메서드
+    @PostConstruct
+    public void init() {
+        secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 로그인 성공시 호출되는 함수
+    public String createToken(Long userId, String username) {
 
         Date now = new Date();
-
-        Date expiredDate = new Date(now.getTime() + expiration);
+        Date expireDate = new Date(now.getTime() + expiration);
 
         return Jwts.builder()
+                // 사용자 정보, 사용자 PK, 토큰 발급시간, 만료 시간
                 .subject(username)
                 .claim("userId", userId)
                 .issuedAt(now)
-                .expiration(expiredDate)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .expiration(expireDate)
+                .signWith(secretKey)
                 .compact();
     }
 
